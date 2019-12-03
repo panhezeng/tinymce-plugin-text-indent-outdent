a45faa1f0962477727e3dc9fc7f24b4971da8c63
Pan Hezeng <panhezeng@gmail.com>
2019-12-03 18:15:59 +0800

    init

--- README.md
+++ README.md
@@ -1,3 +1,31 @@
-# tinymcePluginTextIndent TinyMCE Plugin
+# TinyMCE Plugin
 
-Welcome stranger! This is a repo containing the tinymcePluginTextIndent TinyMCE plugin.
+## 示例
+
+[点击预览](https://panhezeng.github.io/tinymce-plugin-text-indent-outdent/)
+
+官方的 indent 不是 text-indent，于是自己写了一个。
+
+基于官方 indent 相关代码开发
+
+pluginTextIndentOutdent 如果不传，默认值是 2px
+
+使用
+
+```javascript
+tinymce.init({
+  selector: "textarea.tinymce",
+  plugins: "tinymce-plugin-text-indent-outdent",
+  toolbar: "text-indent | text-outdent",
+  pluginTextIndentOutdent: "30px"
+});
+```
+
+## 编译
+
+```bash
+
+# 发版
+npm set registry https://registry.npmjs.org/ && npm set @panhezeng:registry https://registry.npmjs.org/ && npm version patch && npm publish --access public && npm set registry https://registry.npm.taobao.org/ && npm set @panhezeng:registry https://registry.npm.taobao.org/
+
+```
---
+++ example/build.js
@@ -0,0 +1,35 @@
+const shell = require("shelljs");
+const minify = require("html-minifier").minify;
+
+const path = require("path");
+
+function resolve(dir) {
+  return path.join(__dirname, "..", dir);
+}
+
+function build(path) {
+  const dir = resolve(path);
+  shell.rm("-rf", dir);
+  shell.mkdir("-p", dir);
+  shell.cp(
+    resolve("dist/tinymce-plugin-text-indent-outdent/plugin.min.js"),
+    dir
+  );
+  shell
+    .echo(
+      minify(shell.cat(resolve("example/template.html")), {
+        collapseWhitespace: true,
+        removeComments: true,
+        removeOptionalTags: true,
+        removeRedundantAttributes: true,
+        removeScriptTypeAttributes: true,
+        removeTagWhitespace: true,
+        useShortDoctype: true,
+        minifyCSS: true,
+        minifyJS: true
+      })
+    )
+    .to(resolve("docs/index.html"));
+}
+
+build("docs");
