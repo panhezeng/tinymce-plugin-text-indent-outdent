d55f60363754a9997e0575e71f323fb34c76e8a9
Pan Hezeng <panhezeng@gmail.com>
2019-12-03 19:29:37 +0800

    update

--- src/demo/ts/Demo.ts
+++ src/demo/ts/Demo.ts
@@ -1,12 +1,12 @@
-import Plugin from "../../main/ts/Plugin";
+import Plugin from '../../main/ts/Plugin';
 
 declare let tinymce: any;
 
 Plugin();
 
 tinymce.init({
-  selector: "textarea.tinymce",
-  plugins: "code text-indent-outdent",
-  toolbar: "text-indent | text-outdent | indent | code",
-  pluginTextIndentOutdent: "30px"
+  selector: 'textarea.tinymce',
+  plugins: 'code text-indent-outdent',
+  toolbar: 'text-indent | text-outdent | indent | code',
+  pluginTextIndentOutdent: '30px'
 });
