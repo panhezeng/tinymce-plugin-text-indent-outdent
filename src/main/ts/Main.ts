758d4e48a5a637593c399d365cea058089812a74
Pan Hezeng <panhezeng@gmail.com>
2019-12-09 10:37:56 +0800

    del

--- src/main/ts/Main.ts
+++
@@ -1,9 +0,0 @@
-import Plugin from './Plugin';
-
-Plugin();
-
-/*******
- * DO NOT EXPORT ANYTHING
- *
- * IF YOU DO ROLLUP WILL LEAVE A `tinymce-plugin-text-indent-outdent` GLOBAL ON THE PAGE
- *******/
